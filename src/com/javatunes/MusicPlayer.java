2b068cc7cd96047eae13c7be133e3ff21012bfe4
kathyle-dev <69046536+kathyle-dev@users.noreply.github.com>
2021-02-12 19:51:00 -0500

    testing commits and branch switching

--- src/com/javatunes/MusicPlayer.java
+++ src/com/javatunes/MusicPlayer.java
@@ -1,5 +1,14 @@
 package com.javatunes;
 
+import org.w3c.dom.ls.LSOutput;
+
 public class MusicPlayer {
+    //FIELDS
+    Catalog songList;
+    PlayerControls controls;
+
+    public class AudioFile{
+
+    }
 
 }
