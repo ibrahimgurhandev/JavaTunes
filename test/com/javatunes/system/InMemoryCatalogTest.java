c244944b252db5137ee71fee909d652fcae99a87
ibrahimgurhandev <ibrahimgurhan.dev@gmail.com>
2021-02-17 17:04:14 -0500

    created tests, converted hardcoded areas to dynamic

--- test/com/javatunes/system/InMemoryCatalogTest.java
+++ test/com/javatunes/system/InMemoryCatalogTest.java
@@ -1,29 +1,40 @@
 package com.javatunes.system;
 
+import org.junit.Before;
 import org.junit.Test;
 
 import java.util.Collection;
 
 import static org.junit.Assert.assertEquals;
 import static org.junit.Assert.assertNotNull;
-   /*
-public class CatalogTest {
+   
+public class InMemoryCatalogTest {
+    //FIXTURES
+    Catalog catalog;
+    
+    @Before
+    public void setUp() throws IOException {
+        catalog = Catalog.get();
+    }
 
-     * TEST FOR CATALOG METHODS
-     */
-    /*
+    @Test
+    public void load_shouldReturnCorrectSize_whenInstantiated()throws IOException{
+        InMemoryCatalog catalog2 = new InMemoryCatalog("data/song-data.csv");
+        assertEquals(12, catalog2.getCatalog().size());
+    }
+
     @Test
     public void findByGenre_shouldReturnACollection_whenMatchFound() {
-        Collection<Song> songs = Catalog.findByGenre(Genre.POP);
+        Collection<Song> songs = catalog.findByGenre(Genre.POP);
         assertNotNull(songs); //verify is not null
-        assertEquals(1, songs.size()); //should only have one Song
+        assertEquals(2, songs.size()); //should only have two songs -- madonna &christina
         for(Song song: songs){
             assertEquals(Genre.POP, song.getGenre());
         }
     }
     @Test
     public void getSongs_shouldReturnAllSongs() {
-        Collection<Song> songs = Catalog.getSongs();
+        Collection<Song> songs = catalog.getSongs();
         assertNotNull(songs); //verify is not null
         assertEquals(2, songs.size()); //should only have one Song
     }
@@ -35,4 +46,3 @@ public class CatalogTest {
         tvs.add(new Song("-1", "NOT AN ARTIST", "NOT A TITLE", Genre.POP, "NOT A REAL URL"));
     }
 }
-*/
